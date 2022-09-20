854e0e3e338be5c48568134ae03523155fb7afaf
jojuyeong03 <jojuyeong03@gmail.com>
2022-09-20 19:51:24 +0900

    2022/09/20 자습

--- Book1/src/Ch12/Member.java
+++ Book1/src/Ch12/Member.java
@@ -3,10 +3,10 @@ package Ch12;
  * 날짜 : 2022/09/13
  * 이름 : 조주영
  * 내용 : Member 클래스 정의하기
- * 페이지 : 407
+ * 페이지 : 407, 427
  * 
  */
-public class Member {
+public class Member implements Comparable<Member> {
 	private int memberId;		// 회원 아이디
 	private String memberName;	// 회원 이름
 	
@@ -34,6 +34,28 @@ public class Member {
 	@Override
 	public String toString() {
 		return memberName + " 회원님의 아이디는 " + memberId + "입니다";
+	}
+	
+	@Override
+	public int hashCode() {
+		return memberId;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member member = (Member)obj;
+			if (this.memberId == member.memberId)
+				return true;
+			else
+				return false;
+		}
+		return false;
+	}
+
+	@Override
+	public int compareTo(Member member) {
+		return (this.memberId - member.memberId);
 	}
 
 }
