2f1e85661929fa528863b83bbf3c92a241f6700e
juyeong950729 <kikyou950729@gmail.com>
2022-09-28 10:08:48 +0900

    2022/09/28 집

--- Backjoon/src/step04/_05.java
+++ Backjoon/src/step04/_05.java
@@ -1,4 +1,7 @@
 package step04;
+
+import java.util.Scanner;
+
 /*
  * 날짜 : 2022/09/07
  * 이름 : 조주영
@@ -6,5 +9,13 @@ package step04;
  * 
  */
 public class _05 {
-
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		}
+		
+	
 }
--- Backjoon/src/step04/_06.java
+++ Backjoon/src/step04/_06.java
@@ -3,7 +3,7 @@ package step04;
 import java.util.Scanner;
 
 /*
- * 날짜 : 2022/09/07
+ * 날짜 : 2022/09/28
  * 이름 : 조주영
  * 내용 : 백준 4단계 6번 문제, 평균은 넘겠지
  * 
@@ -15,17 +15,32 @@ public class _06 {
 		Scanner sc = new Scanner(System.in);
 		
 		int c = sc.nextInt();
-		double sum = 0;
-		double avg = 0;
 		
-		for (int i=0; i<=c; i++) {
+		for (int i=0; i<c; i++) {
+			
+			double sum = 0;
+			
 			int n = sc.nextInt();
-			for (int j=0; j<=n; j++) {
-				double score = sc.nextDouble();
-				sum += score;
+			double[] arr1 = new double[n];
+			
+			for (int j=0; j<n; j++) {
+				arr1[j] = sc.nextDouble();
+				sum += arr1[j];
 			}
-			avg = sum / n;
-			System.out.println(Math.round(avg));
+			
+			double count = 0;
+			double avg = sum / n;
+			
+			
+			for (int k=0; k<n; k++) {
+				if (arr1[k] > avg) {
+					count++;
+				}
+			}
+			
+			double rate = (count / n)*100;
+			System.out.printf("%.3f%%\n", rate);
+				
 		}
 		
 	}
