fe4cfae388d7b923ee1ec6a02b8bde4286b970ff
jojuyeong03 <jojuyeong03@gmail.com>
2022-10-17 19:24:30 +0900

    2022/10/17 자습

---
+++ Backjoon/src/step06/_05.java
@@ -0,0 +1,28 @@
+package step06;
+
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/10/17
+ * 이름 : 조주영
+ * 내용 : 백준 6단계 5번 문제, 단어 공부
+ * 
+ */
+public class _05 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String n = sc.next();
+		byte n1;
+		
+		for (int i=0; i<n.length(); i++) {
+			n1 = (byte) n.charAt(i);
+			if (n1 >= 65 && n1 <=90) {
+				
+			}
+		}
+		
+	}
+
+}
