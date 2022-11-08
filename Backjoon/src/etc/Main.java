f090bf71d69cdfb00563f6b73e2d850848fa8c2f
jojuyeong03 <jojuyeong03@gmail.com>
2022-11-08 19:22:26 +0900

    2022/11/08 자습

--- Backjoon/src/etc/Main.java
+++ Backjoon/src/etc/Main.java
@@ -7,6 +7,24 @@ public class Main {
 			
 		Scanner sc = new Scanner(System.in);
 		
+		int n = sc.nextInt();
+		int arr1[] = new int[n];
+		
+		for (int i=0; i<n; i++) {
+			arr1[i] = sc.nextInt();
+		}
+		
+		int v = sc.nextInt();
+		int count = 0;
+		
+		for (int j=0; j<n; j++) {
+			if (arr1[j] == v) {
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+		
 		
 	}
 }
