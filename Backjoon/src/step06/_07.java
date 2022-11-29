b17e2cd0de22b64534784538a65a6c67b4a10b48
jojuyeong03 <jojuyeong03@gmail.com>
2022-11-29 19:13:12 +0900

    2022/11/29 자습

--- Backjoon/src/step06/_07.java
+++ Backjoon/src/step06/_07.java
@@ -17,10 +17,22 @@ public class _07 {
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
 		
-		int a1 = num1 - (num1 % 100);
-		int a2 = (num1 - (num1 % 100 * 100)) % 10;
-		int b1 = num2 % 100;
-		int b2 = num2 % 10;
+		int a1 = (num1 - (num1 % 100)) / 100;
+		int a2 = (num1 - (a1*100)) / 10;
+		int a3 = num1 % 10;
+		int b1 = (num2 - (num2 % 100)) / 100;
+		int b2 = (num2 - (b1*100)) / 10;
+		int b3 = num2 % 10;
+		
+		num1 = a1 + (a2 * 10) + (a3 * 100);
+		num2 = b1 + (b2 * 10) + (b3 * 100);
+		
+		
+		if (num1 >= num2) {
+			System.out.println(num1);
+		} else if (num2 > num1) {
+			System.out.println(num2);
+		} 
 		
 		
 	}
---
+++ Backjoon/src/step08/_01.java
@@ -0,0 +1,23 @@
+package step08;
+
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/11/29
+ * 이름 : 조주영
+ * 내용 : 백준 8단계 1번 문제, 손익분기점
+ * 
+ */
+public class _01 {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long c = sc.nextLong();
+		
+	}
+	
+}
